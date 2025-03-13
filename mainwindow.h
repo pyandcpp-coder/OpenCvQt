5412d7e84f60baf3e4154e80fff3f63141fafd53
Yash Gyanendra Tiwari <yashtiwari9182@gmail.com>
2025-03-13 16:57:38 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,68 +1,4 @@
-// #ifndef MAINWINDOW_H
-// #define MAINWINDOW_H
 
-// #include <QMainWindow>
-// #include <QTimer>
-// #include <QLabel>
-// #include <vector>
-// #include <opencv2/opencv.hpp>
-// #include <opencv2/dnn.hpp>
-// #include <opencv2/dnn/all_layers.hpp>
-
-// // Structure to store detection information
-// struct Detection {
-//     int classId;
-//     float confidence;
-//     cv::Rect box;
-//     cv::Point center;
-//     std::string className;
-// };
-
-// class MainWindow : public QMainWindow
-// {
-//     Q_OBJECT
-
-// public:
-//     MainWindow(QWidget *parent = nullptr);
-//     ~MainWindow();
-
-// private slots:
-//     void processFrame();
-
-// private:
-//     // UI Components
-//     QLabel *imageLabel;
-//     QTimer *timer;
-
-//     // OpenCV Components
-//     cv::VideoCapture cap;
-//     cv::dnn::Net net;
-//     std::vector<std::string> classes;
-
-//     // Parameters for detection
-//     float confThreshold;
-//     float nmsThreshold;
-//     int inpWidth;
-//     int inpHeight;
-
-//     // Calibration variables
-//     double pixelsPerCm;
-//     bool isCalibrated;
-//     double referenceWidthCm;
-//     bool calibrationMode;
-
-//     // Functions
-//     std::vector<std::string> getOutputsNames(const cv::dnn::Net& net);
-//     std::vector<std::string> loadClassNames(const std::string& filename);
-//     void drawPred(const Detection& detection, cv::Mat& frame);
-//     double calculatePixelDistance(const cv::Point& p1, const cv::Point& p2);
-//     double pixelToRealDistance(double pixelDistance, double pixelsPerCm);
-//     double calibratePixelsPerCm(int referenceWidthPixels, double referenceWidthCm);
-//     void setupNetwork();
-//     void setupUI();
-// };
-
-// #endif // MAINWINDOW_H
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -73,24 +9,22 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 
-// Struct to store orientation information
 struct OrientationInfo {
-    float angle;             // Angle in degrees
-    std::string direction;   // Direction description (e.g., "Horizontal", "Vertical")
-    double confidence;       // Confidence in orientation detection
+    float angle;             
+    std::string direction;   
+    double confidence;       
 };
 
-// Struct to store detection information
 struct Detection {
     int classId;
     float confidence;
     cv::Rect box;
     cv::Point center;
     std::string className;
-    double width;            // Width in cm
-    double height;           // Height in cm
+    double width;            
+    double height;          
     OrientationInfo orientation;
-    double horizontalAngle;  // Angle with respect to horizontal
+    double horizontalAngle;  
 };
 
 class MainWindow : public QMainWindow
